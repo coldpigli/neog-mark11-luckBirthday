f60465ecad4ce946d093a97e84bf091db64fd0a2
coldpigli <piyushdas98@gmail.com>
2021-09-01 00:43:55 +0530

    func added works fine

--- src/App.js
+++ src/App.js
@@ -3,17 +3,35 @@ import React, { useState } from 'react';
 function App() {
   
   const [messLucky, setMessLucky] = useState("");
+  const[datepicked, setDatePicked] = useState();
+  const[luckyNumber, setLuckyNumber] = useState();
+
+
+  const checkBirthday = () => {
+    let strippedString = datepicked;
+    strippedString = strippedString.replaceAll('-','');
+    let sumOfDigits = 0;
+    for(let i=0; i<strippedString.length; i++){
+      sumOfDigits += Number(strippedString[i]);
+    }
+    if(sumOfDigits%luckyNumber === 0){
+      setMessLucky("Congratulations!! Your birthday is Lucky!!")
+    }
+    else{
+      setMessLucky("Umm..Sorry. Birthday isn't lucky. But you make your own luck")
+    }
+  }
   
   return (
   <div className = "container">
     <h1>Check if your Birthday is Lucky or not</h1>
     <iframe className="gif" src="https://giphy.com/embed/bJO0LN9AMKtAbIDF34" width="200" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     <label htmlFor="datePicker">Enter your Date-Of-Birth</label>
-    <input id = "datePicker" type = "date"></input>
+    <input id = "datePicker" type = "date" onChange = {(e)=>{setDatePicked(e.target.value)}}></input>
     <label htmlFor="luckyNumber">Enter your Lucky Number</label>
-    <input id = "luckyNumber" type = "number"></input>
+    <input id = "luckyNumber" type = "number" onChange = {(e)=>{setLuckyNumber(e.target.value)}}></input>
     <h3>{messLucky}</h3>
-    <button>I'm feeling lucky</button>
+    <button onClick = {checkBirthday}>I'm feeling lucky</button>
   </div>
     )
 }
