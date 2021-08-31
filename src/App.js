d8b1be9ace987d71abf0456e98981132bcd5fed6
coldpigli <piyushdas98@gmail.com>
2021-09-01 00:16:44 +0530

    completed UI

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { useState } from 'react';
 
 function App() {
   
+  const [messLucky, setMessLucky] = useState("");
   
   return (
-  <h2>hello</h2>
+  <div className = "container">
+    <h1>Check if your Birthday is Lucky or not</h1>
+    <iframe className="gif" src="https://giphy.com/embed/bJO0LN9AMKtAbIDF34" width="200" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    <label htmlFor="datePicker">Enter your Date-Of-Birth</label>
+    <input id = "datePicker" type = "date"></input>
+    <label htmlFor="luckyNumber">Enter your Lucky Number</label>
+    <input id = "luckyNumber" type = "number"></input>
+    <h3>{messLucky}</h3>
+    <button>I'm feeling lucky</button>
+  </div>
     )
 }
 
